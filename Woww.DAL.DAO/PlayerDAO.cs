dfcfc958848ffd6dad6149fa0b8d4187efae9249
steveBstorm <steve.lorent@bstorm.be>
2021-12-07 12:08:51 +0100

    + playerDAO

---
+++ Woww.DAL.DAO/PlayerDAO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Woww.DAL.DTO;
+
+namespace Woww.DAL.DAO
+{
+    public class PlayerDAO
+    {
+        private const string _connectionString = @"Data Source=DESKTOP-RGPQP6I\TFTIC2019;Initial Catalog=WoWW;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        private Player Convert(IDataRecord reader)
+        {
+            return new Player
+            {
+                Id = (int)reader["Id"],
+                Name = (string)reader["name"],
+                Email = (string)reader["email"],
+                FK_Team = (int)reader["FK_Team"]
+            };
+        }
+
+        public bool Register(Player p)
+        {
+            using(SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Player (name, email, password, FK_Team) " +
+                        "VALUES (@name, @email, @pwd, @fk)";
+
+                    cmd.Parameters.AddWithValue("name", p.Name);
+                    cmd.Parameters.AddWithValue("email", p.Email);
+                    cmd.Parameters.AddWithValue("pwd", p.Password);
+                    cmd.Parameters.AddWithValue("fk", p.FK_Team);
+
+                    return cmd.ExecuteNonQuery() == 1;
+                }
+            }
+        }
+
+        public Player Login(string email, string pwd)
+        {
+            using(SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using(SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Player WHERE email = @email AND password = @pwd";
+                    cmd.Parameters.AddWithValue("email", email);
+                    cmd.Parameters.AddWithValue("pwd", pwd);
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read()) return Convert(reader);
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
--- Woww/Program.cs
+++ Woww/Program.cs
@@ -8,9 +8,9 @@ namespace Woww
     {
         static void Main(string[] args)
         {
-            TeamDAO teamDAO = new TeamDAO();
+            //TeamDAO teamDAO = new TeamDAO();
 
-            #region Création de team
+            //#region Création de team
             //Team t1 = new Team();
             //t1.Name = "WAD 21";
 
@@ -21,14 +21,34 @@ namespace Woww
             //else Console.WriteLine("Un problème est survenu");
 
             //if (teamDAO.Create(t2)) Console.WriteLine("Team 2 créée");
-            //else Console.WriteLine("Un problème est survenu"); 
-            #endregion
+            //else Console.WriteLine("Un problème est survenu");
+            //#endregion
 
-            foreach (Team t in teamDAO.GetAll())
-            {
-                Console.WriteLine("ID : {0} -- Nom : {1}", t.Id, t.Name);
+            //foreach (Team t in teamDAO.GetAll())
+            //{
+            //    Console.WriteLine("ID : {0} -- Nom : {1}", t.Id, t.Name);
+            //}
+
+            PlayerDAO playerDAO = new PlayerDAO();
+
+            //Player newP = new Player();
+            //newP.Name = "steve";
+            //newP.Email = "steve@test.com";
+            //newP.Password = "1234";
+            //newP.FK_Team = 1;
+
+            //if (playerDAO.Register(newP)) Console.WriteLine("Inscription réussie");
+            //else Console.WriteLine("ça à foiré quelque part");
+
+            Player currentPlayer = playerDAO.Login("steve@test.com", "1234");
+
+            if (currentPlayer != null)
+            { 
+                Console.WriteLine("Bonjour {0}", currentPlayer.Name);
+                Console.WriteLine("Mot de passe : {0}", currentPlayer.Password);
+                Console.WriteLine("Equipe : {0}", currentPlayer.FK_Team);
             }
-
+            else Console.WriteLine("Erreur de mot de passe ou d'adresse email");
 
         }
     }
